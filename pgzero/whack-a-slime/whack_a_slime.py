aadf7ba8b27301025d4398f5113ffd415f47834c
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-08-28 22:10:18 -0500

    commit

--- pgzero/whack-a-slime/whack_a_slime.py
+++ pgzero/whack-a-slime/whack_a_slime.py
@@ -4,46 +4,43 @@ from random import randint
 # screen size
 WIDTH = 800
 HEIGHT = 600
-mouse_pos = (0, 0)
 
 # actors
 slime = Actor('slime', pos=(400, 300))
+slime.wait_time = 60
+slime.counter = slime.wait_time
 
-hole1 = Actor('hole')
-hole1.x = 200
-hole1.y = 300
+hole1 = Actor('hole', pos=(200, 300))
+hole2 = Actor('hole', pos=(600, 300))
 
-hole2 = Actor('hole')
-hole2.x = 600
-hole2.y = 300
+# global variable
+score = 0
 
-help(Actor)
-
-
-# runs 60 times per second
 def draw():
+    screen.fill("green")
     slime.draw()
     hole1.draw()
     hole2.draw()
 
 
-def on_mouse_move(pos):
-    screen.draw.text(f"({pos[0]}, {pos[1]})", (pos[0] + 10, pos[1]))
-
-
 # runs 60 times per second
 def update():
-    rand_num = randint(1, 2)
-    if rand_num == 1:
-        slime.x = hole1.x
-        slime.y = hole1.y - 25
-    elif rand_num == 2:
-        slime.x = hole2.x
-        slime.y = hole2.y - 25
-    screen.draw.text("hello", (10, 10))
-    if keyboard.d:
+    slime.counter = slime.counter - 1
+    if slime.counter <= 0:
+        slime.counter = slime.wait_time
+        rand_num = randint(1, 2)
+        if rand_num == 1:
+            slime.x = hole1.x
+            slime.y = hole1.y - 25
+        elif rand_num == 2:
+            slime.x = hole2.x
+            slime.y = hole2.y - 25
 
-        slime.x += slime.speed
+
+def on_mouse_down(pos):
+    if slime.collidepoint(pos):
+        score += 1
+        print(score)
 
 
 # Must be the last line
