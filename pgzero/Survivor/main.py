ac9fbb670514a88770acb4703e5cbe0d39955800
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-12-17 12:02:09 -0600

    commit

--- pgzero/Survivor/main.py
+++ pgzero/Survivor/main.py
@@ -8,11 +8,21 @@ TITLE = "Survivor Game"
 # create the background actor and store in a variable
 bg = Actor("dungeon1")
 
+# create player Actor
+player = Actor("green_guy")
 
+
+
+###########################Built in functions###############
 # define the draw function (built in to pygame zero automatically)
 def draw():
     bg.draw()
+    player.draw()
 
+
+# define the update function (runs a forever loop ---- while True) runs 60 times a second
+def update():
+    pass
 
 # last line of code
 go()
