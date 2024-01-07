308c4cfe3223f9bf190dad14a8da757aa0af77d5
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2024-01-07 12:22:58 -0600

    movement and boundiung

--- pgzero/Survivor/main.py
+++ pgzero/Survivor/main.py
@@ -1,4 +1,7 @@
+# flake8: noqa
+
 from pgzrun import go
+from pgzhelper import Actor
 
 # screen setup
 WIDTH = 1500
@@ -9,10 +12,43 @@ TITLE = "Survivor Game"
 bg = Actor("dungeon1")
 
 # create player Actor
-player = Actor("green_guy")
+player = Actor("woman")
+player.pos = (300, 300)
+player.scale = 4
+player.speed = 5
 
 
-# ########################## Built in functions ##################### #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~ Our Functions ~~~~~~~~~~~~~~~~~~~~~~~~~ #
+def move_player():
+    # move to the left
+    if keyboard.a:
+        player.x -= player.speed
+    # move to the right
+    elif keyboard.d:
+        player.x += player.speed
+    # move to the up
+    elif keyboard.w:
+        player.y -= player.speed
+    # move to the down
+    elif keyboard.s:
+        player.y += player.speed
+
+
+def bound_player():
+    if player.left < 100:
+        player.left = 100
+    if player.y < 100:
+        player.y = 100
+
+
+# ************************** Built in functions ************************** #
+
+# runs when any mouse button is pressed
+def on_mouse_down(pos):
+    print(pos)
+
+
+# runs everytime update does
 def draw():
     ''' define the draw function (built in to pygame zero automatically) '''
     bg.draw()
@@ -21,7 +57,8 @@ def draw():
 
 # define the update function (runs a forever loop ---- while True) runs 60 times a second
 def update():
-    pass
+    move_player()
+    bound_player()
 
 
 # last line of code
