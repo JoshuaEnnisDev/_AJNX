90e134bf040a8b1f68641e75f78222bbc6151b13
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2024-01-21 19:28:30 -0600

    commit

---
+++ Pure Python/Introdue_Yourself/main.py
@@ -0,0 +1,12 @@
+# flake8:noqa
+
+# Data Types
+    # String -- anything inside of quotes 
+"Kemo"
+"Cabbage"
+    # Integers -- Positive or negative Whole numbers (No decimals)
+3
+-12
+    
+# Functions -- Performs an action -- Usually a verb end in ()
+print("Hola, me llamo es Josh")
