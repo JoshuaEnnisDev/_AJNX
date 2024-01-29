46d757daf33b7e42f3da4da3aeaca2a1b1c45f80
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2024-01-28 19:20:15 -0600

    commit

--- Pure Python/Introduce_Yourself/main.py
+++ Pure Python/Introduce_Yourself/main.py
@@ -7,6 +7,12 @@
     # Integers -- Positive or negative Whole numbers (No decimals)
 3
 -12
-    
+
 # Functions -- Performs an action -- Usually a verb end in ()
 print("Hola, me llamo es Josh")
+
+# variables give data and store data for later use.
+name = "Kemo"
+fav_food = "Cabbage"
+
+print("My favorite food is " )
