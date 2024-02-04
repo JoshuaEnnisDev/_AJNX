2ff369ba8ea3c5877ded3a26ccdaad9c370b40f3
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2024-02-04 11:50:02 -0600

    madlibs print inuit

--- Pure Python/Introduce_Yourself/main.py
+++ Pure Python/Introduce_Yourself/main.py
@@ -1,10 +1,10 @@
 # flake8:noqa
 
 # Data Types
-    # String -- anything inside of quotes 
+# String -- anything inside of quotes 
 "Kemo"
 "Cabbage"
-    # Integers -- Positive or negative Whole numbers (No decimals)
+# Integers -- Positive or negative Whole numbers (No decimals)
 3
 -12
 
@@ -14,5 +14,18 @@ print("Hola, me llamo es Josh")
 # variables give data and store data for later use.
 name = "Kemo"
 fav_food = "Cabbage"
+age = 37
 
-print("My favorite food is " )
+print("My favorite food is " + fav_food)
+print("I am " + str(age) + " years old")
+
+# f strings allow you to put variables directly in your string
+print(F"I am {age} years old and my name is {name}. I also like {fav_food}")
+
+
+
+
+
+# Multiline stings
+
+paragraph = "anything in here is string"
---
+++ Pure Python/Madlibs/main.py
@@ -0,0 +1,7 @@
+
+# We need a way to get answers from the user.
+
+# the input() function allows us to do that!
+
+input("Enter a noun: ")
+
